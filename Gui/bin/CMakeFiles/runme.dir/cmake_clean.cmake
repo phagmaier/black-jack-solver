17b47d96a9163c289bad2028ebb4384e903f8007
phagmaier <parkerhagmaier@gmail.com>
2024-12-10 18:06:16 -0800

    Finished GUI

---
+++ Gui/bin/CMakeFiles/runme.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runme.dir/link.d"
+  "CMakeFiles/runme.dir/src/main.cpp.o"
+  "CMakeFiles/runme.dir/src/main.cpp.o.d"
+  "runme"
+  "runme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Gui/bin/CMakeFiles/runme.dir/link.d
@@ -0,0 +1,31 @@
+runme: \
+  /usr/lib/libm.so \
+  /usr/lib/libm.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libc.so \
+  /usr/lib/libc.so \
+  /usr/lib/libgcc_s.so \
+  /usr/lib/libgcc_s.so \
+  /tmp/ccJteQY5.ltrans0.ltrans.o \
+  /usr/lib/ld-linux-x86-64.so.2
+
+/usr/lib/libm.so:
+
+/usr/lib/libm.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libc.so:
+
+/usr/lib/libgcc_s.so:
+
+/usr/lib/libgcc_s.so:
+
+/tmp/ccJteQY5.ltrans0.ltrans.o:
+
+/usr/lib/ld-linux-x86-64.so.2:
